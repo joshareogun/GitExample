7f1d21bd7fba20196b6b7f5dbc69a66f853a7387
Joshua Areogun <jareogun@gmail.com>
2013-07-25 15:43:36 +0100

    Initial Commit

---
+++ GitExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitExample
+//
+//  Created by Joshua Areogun on 7/25/13.
+//  Copyright (c) 2013 Joshua Areogun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitExample
+//
+//  Created by Joshua Areogun on 7/25/13.
+//  Copyright (c) 2013 Joshua Areogun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
